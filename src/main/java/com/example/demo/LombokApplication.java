ad7a6ae701b942f21d5a0db704194f551e4051bd
Alex Pask <pask200@gmail.com>
2017-07-15 17:33:50 +0100

    M8

--- src/main/java/com/example/demo/LombokApplication.java
+++ src/main/java/com/example/demo/LombokApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LombokApplication {
+	
+	// M8
 
 	public static void main(String[] args) {
 		SpringApplication.run(LombokApplication.class, args);
